1bf021caf17bfe533e8789cb448eaa09bf105731
Claus Rasmussen <claus.rasmussen@samesystem.com>
2018-01-13 14:15:56 +0100

    Initial import

---
+++ lib/query_log_tools.rb
@@ -0,0 +1,5 @@
+require "query_log_tools/version"
+
+module QueryLogTools
+  # Your code goes here...
+end
---
+++ spec/query_log_tools_spec.rb
@@ -0,0 +1,7 @@
+describe QueryLogTools do
+  it 'should have a version number' do
+    QueryLogTools::VERSION.should_not == nil
+  end
+
+  it 'should do something useful'
+end
